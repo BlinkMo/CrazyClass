6f8d636642f8b5d4142f72ce1d609c16e1a8d9c6
BlinkMo <Blink@mengmodeMacBook-Pro.local>
2016-06-04 17:19:21 +0800

    add q2

--- src/Chapter_1.java
+++ src/Chapter_1.java
@@ -16,10 +16,30 @@ public class Chapter_1 {
 			System.out.print("\n");
 		}
 	}
+	
+	public static void question2(int count)
+	{
+		for(int i = 0 ; i < count; i++)
+		{
+			for(int j = 1 ; j < count - i ; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k = 0 ; k < 2*i + 1 ; k++)
+			{
+				System.out.print("*");
+			}
+			for(int l = 1 ; l < count - i ; l++)
+			{
+				System.out.print(" ");
+			}
+			System.out.print("\n");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		question1();
+		question2(4);
 
 	}
 
